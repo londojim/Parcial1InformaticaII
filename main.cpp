d0f82c84fbdb69835fe5673f9c24dc144ddeefaf
dalj <dalj@enigma.localdomain>
2020-10-17 16:54:09 -0500

    haciedno las funciones, habia olvidado hacer los commit

--- main.cpp
+++ main.cpp
@@ -2,30 +2,93 @@
 ################################## PARCIAL_1 INFORMATICA II ##################################
  ############################# DIEGO ALEJANDRO LONDOÑO JIMENEZ ##############################
   #########################################71377279#########################################
-
-
-git remote add origin https://github.com/londojim/PRuebaaa.git
-git branch -M main
-git push -u origin main
-
-
-
-
-
-
-
-
 */
 
-
-
-
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+
+int dispOfenDananCaDefen();
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int opcion = 0;
+
+    do
+    {
+
+        cout << "****************** MENU DE OPCIONES ******************" << endl
+             << endl;
+        cout << "Seleccione la opcion que desee usar" << endl
+             << endl;
+        cout << "0  salir" << endl;
+        cout << "1  Generar disparos (al menos tres) ofensivos que comprometan la integridad del cañón defensivo" << endl;
+        cout << "2  Generar disparos (al menos tres) defensivos que comprometan la integridad del cañón ofensivo." << endl;
+        cout << "3  Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el cañón defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido" << endl;
+
+        cout << "4  Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los cañones defensivo y ofensivo puedan ser destruidos." << endl;
+
+        cout << endl;
+        cout << "Ingrese su opcion :";
+        cin >> opcion;
+
+        enum CasosEnunciados
+        {
+            salir,
+            integridad_canon_defensivo,
+            integridad_canon_ofensivo,
+            caracteres_numero,
+            fjadlksjfklas
+        };
+
+        switch (opcion)
+        {
+
+        case salir:
+            //opcion 0
+            cout << endl
+                 << "Gracias por usar el programa, no olvide darle like :)" << endl
+                 << endl;
+            break;
+
+        case        integridad_canon_defensivo      :
+            //opcion 1
+            cout<<cos(60);
+            break;
+
+        case integridad_canon_ofensivo:
+            //opcion 2
+
+            break;
+
+        case caracteres_numero:
+            //opcion 3
+
+            break;
+
+        case fjadlksjfklas:
+            //opcion 4
+
+            break;
+
+        default:
+            cout << "Opcion no valida" << endl;
+            break;
+        } //fin  switch
+
+    } while (opcion != 0);
+
     return 0;
+}
+
+
+int dispOfenDananCaDefen(){
+
+
+
+
+
+
 }
