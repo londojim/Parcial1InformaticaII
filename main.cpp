db4fde52e9d5b53a6b43c84210818c8284bf3a04
dalj <dalj@enigma.localdomain>
2020-10-17 14:27:09 -0500

    Se termina de leer el primer parcial

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+################################## PARCIAL_1 INFORMATICA II ##################################
+ ############################# DIEGO ALEJANDRO LONDOÑO JIMENEZ ##############################
+  #########################################71377279#########################################
+
+
+git remote add origin https://github.com/londojim/PRuebaaa.git
+git branch -M main
+git push -u origin main
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
